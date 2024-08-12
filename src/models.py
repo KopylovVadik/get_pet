0acfed773e02aa9ab81623704573a54de8370f8c
vadimkopylov <kopylov.ddut@gmail.com>
2024-08-12 14:20:13 +0300

    alembic

---
+++ src/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, String, Integer, DateTime, Date, func, ForeignKey
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+
+class Owner(Base):
+    __tablename__ = 'owners'
+    id = Column(Integer, primary_key=True, unique=True)
+    first_name = Column(String, comment='Имя')
+    last_name = Column(String, comment='Фамилия')
+    phone_number = Column(String, unique=True, comment='Номер телефона')
+    email = Column(String, unique=True, comment='Адрес электронной почты')
+    telegram = Column(String, unique=True, nullable=True, comment='Телеграм')
+    created_at = Column(DateTime, default=func.now())
+
+
+class Pet(Base):
+    __tablename__ = 'pets'
+
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String, comment='Кличка')
+    birth_date = Column(Date, comment='Дата рождения')
+    brand = Column(String, nullable=True, comment='Порода')
+    special_feature = Column(String, nullable=True, comment='Особые приметы')
+    owner_id = Column(Integer, ForeignKey('owners.id'))
+    created_at = Column(DateTime, default=func.now())
