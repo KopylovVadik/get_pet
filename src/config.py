3eb234efe12dd06f1663db9bdaa96235a231ffda
vadimkopylov <kopylov.ddut@gmail.com>
2024-08-12 13:05:04 +0300

    create sokcer compose

--- src/config.py
+++ src/config.py
@@ -2,8 +2,11 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    # app
     host: str = "0.0.0.0"
     port: int = 8080
+
+    # router
     api_prefix: str = "/api"
 
 
